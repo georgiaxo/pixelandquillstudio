e6e3c63806b1eaf4ebd7d0b563cb1d5a76ab3e4d
georgiaxo <georgia@pixelandquillstudio.com.au>
2026-02-18 09:33:16 +1000

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,1 @@
-(() => {
-  const header = document.querySelector("header.site-header");
-  const toggle = document.querySelector("[data-nav-toggle]");
-  if (!header || !toggle) return;
 
-  toggle.addEventListener("click", () => {
-    const open = header.getAttribute("data-open") === "true";
-    header.setAttribute("data-open", String(!open));
-  });
-})();
